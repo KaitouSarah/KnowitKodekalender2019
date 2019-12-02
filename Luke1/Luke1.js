a215386478d3cef5e5aa6365d505da0d7e97f7d3
Liv Sarah <sarahansnes@gmail.com>
2019-12-02 15:04:48 +0100

    feat(Luke1): added solution and task description to task 1

--- Luke1/Luke1.js
+++ Luke1/Luke1.js
@@ -1,2 +1,31 @@
-var fs = require("fs");
-var textByLine = fs.readFileSync("C:/Users/lsansn/Desktop/KnowitKodekalender2019/Luke2/dataLuke2.txt").toString('utf-8').split(/\r\n|\r|\n/g);
+const fs = require("fs");
+const availableSheep = fs.readFileSync("C:/Users/lsansn/Desktop/KnowitKodekalender2019/Luke1/dataLuke1.txt").toString('utf-8').split(', ');
+
+let dragonSize = 50;
+let daysAlive = 0;
+let daysHungryDragon = 0;
+let leftoverSheep = 0;
+let alive = true;
+
+while (alive) {
+    let totalSheep = +availableSheep[daysAlive] + +leftoverSheep; 
+    console.log("Total sheep: ", totalSheep, "  Sheep today: ", availableSheep[daysAlive], "  leftovers: ", leftoverSheep, "  Dragon size: ", dragonSize);
+    if (dragonSize <= totalSheep) {
+        daysAlive++;
+        leftoverSheep = totalSheep - dragonSize;
+        dragonSize++;
+        daysHungryDragon = 0;
+    } else {
+        daysHungryDragon++;
+        if (daysHungryDragon == "5") {
+            alive = false;
+            console.log("Days alive: ", daysAlive);
+        } else {
+            dragonSize--;
+            leftoverSheep = 0;
+            daysAlive++;
+        }
+    }
+}
+
+

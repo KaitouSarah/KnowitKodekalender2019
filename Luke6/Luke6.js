3f7fe62634a1cc5b5b6400a482049a9fcd2314a6
Liv Sarah <sarahansnes@gmail.com>
2019-12-08 02:14:26 +0100

    fix(Luke6): minor fix on loops

--- Luke6/Luke6.js
+++ Luke6/Luke6.js
@@ -7,14 +7,14 @@ fs.createReadStream('./Luke6/mush.png')
     .on('parsed', function() {
         let imgArray = new Array();
         imgArray.push(this.data[0], this.data[1], this.data[2], this.data[3]);
-        for (let y = 4; y < (this.width * this.height*4); y += 4) {
+        for (let y = 4; y < this.data.length; y += 4) {
             let a, b, c;
             a = this.data[y] ^ this.data[y-4];
             b = this.data[y+1] ^ this.data[y-3];
             c = this.data[y+2] ^ this.data[y-2];
             imgArray.push(a, b, c, 255);
         }
-        for (let y = 0; y < (this.width * this.height*4); y++) {
+        for (let y = 0; y < this.data.length; y++) {
             this.data[y] = imgArray[y];
         }
         this.pack().pipe(fs.createWriteStream('./Luke6/originalImg.png'));
